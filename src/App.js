2660256f4daaed0d8a3a2dcf19cea4962a3ce448
jorge niglia <jorge.niglia@gmail.com>
2021-10-18 21:00:16 -0300

    hasta maquetación

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import "./App.css";
-
-function App(autor) {
+//import "./App.css";
+import { TodoCounter } from "./TodoCounter";
+import { TodoSearch } from "./TodoSearch";
+import { TodoList } from "./TodoList";
+import { TodoItem } from "./TodoItem";
+import { CreateTodoButton } from "./CreateTodoButton";
+const todos = [
+    { text: "Enviar correo a SHELL", completed: false },
+    { text: "Comprar cadenas de inoxidable", completed: false },
+    { text: "Hacer ensayos con HT", completed: false },
+];
+function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <h1>TODO APP </h1>
-                <p>Autor: {autor.nombre}</p>
-                <p>Año: {autor.children}</p>
-            </header>
-        </div>
+        <>
+            <TodoCounter />
+            <TodoSearch />
+            <TodoList>
+                {todos.map((todo) => (
+                    <TodoItem key={todo.text} text={todo.text} />
+                ))}
+            </TodoList>
+            <CreateTodoButton />
+        </>
     );
 }
-
 export default App;
